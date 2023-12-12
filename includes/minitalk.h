54045b49f5000a46264bd2738121e29cdb1caf75
cefuente <cfuen55010@gmail.com>
2023-12-12 18:14:53 +0100

    switch

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cesar <cesar@student.42.fr>                +#+  +:+       +#+        */
+/*   By: cefuente <cefuente@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/03 16:22:36 by cesar             #+#    #+#             */
-/*   Updated: 2023/12/12 11:15:21 by cesar            ###   ########.fr       */
+/*   Updated: 2023/12/12 17:17:33 by cefuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,5 @@
 # include <unistd.h>
 # include <sys/types.h>
 # include "../libft/libft.h"
-# include <stdio.h> ///////////////
 
 #endif
